f4aa999c650a9ceed28d47d2136dd91d20cdbfdb
luciacantos <luciacantos22@gmail.com>
2023-09-18 00:42:52 +0200

    tabla con resultados

--- caballo.py
+++ caballo.py
@@ -19,3 +19,13 @@ def contar_movimientos(n):
 n = 10
 resultado = contar_movimientos(n)
 print(f"La cantidad de movimientos posibles para {n} es {resultado}")
+
+valores_de_n = [1, 2, 3, 5, 8, 10, 15, 18, 21, 23, 32]
+
+# Crear una tabla para mostrar los resultados
+print("n\tPosibilidades válidas")
+print("-" * 24)
+
+for n in valores_de_n:
+    resultado = contar_movimientos(n)
+    print(f"{n}\t{resultado}")
